b4f7dff6109652d9d316784d22a21ad2603cf191
Artem Evseviev <artem@qrsoft.ru>
2023-05-01 14:33:47 +0300

    theme refactor

--- init.lua
+++ init.lua
@@ -1,13 +1,2 @@
 return {
-  colorscheme = "everforest",
-
-  plugins = {
-    {
-      "neanias/everforest-nvim",
-      as = "everforest",
-      config = function()
-        require("everforest").setup {}
-      end,
-    },
-  },
 }
---
+++ plugins/everforest.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "neanias/everforest-nvim",
+    as = "everforest",
+    config = function()
+      require("everforest").setup {}
+    end,
+  },
+}
